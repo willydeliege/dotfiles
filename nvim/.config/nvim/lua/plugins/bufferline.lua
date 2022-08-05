89f6575d17934a7685c540f4ce8c3ae16997bc16
Frédéric Willem <frederuc.willem@gmail.com>
2022-08-05 18:42:45 +0200

    add close button to bufferline

--- nvim/.config/nvim/lua/plugins/bufferline.lua
+++ nvim/.config/nvim/lua/plugins/bufferline.lua
@@ -15,11 +15,11 @@ bufferline.setup {
     -- as an escape hatch for people who cannot bear it for whatever reason
     indicator_icon = "│",
     -- indicator_icon = "▎",
-    buffer_close_icon = "",
-    -- buffer_close_icon = '',
+    -- buffer_close_icon = "",
+    buffer_close_icon = '',
     modified_icon = "●",
-    close_icon = "",
-    -- close_icon = '',
+    -- close_icon = "",
+    close_icon = '',
     left_trunc_marker = "",
     right_trunc_marker = "",
     --- name_formatter can be used to change the buffer's label in the bufferline.
@@ -35,8 +35,8 @@ bufferline.setup {
     max_name_length = 30,
     max_prefix_length = 30, -- prefix used when a buffer is de-duplicated
     tab_size = 21,
-    diagnostics = false, -- | "nvim_lsp" | "coc",
-    diagnostics_update_in_insert = false,
+    diagnostics = "nvim_lsp", -- false | "nvim_lsp" | "coc",
+    diagnostics_update_in_insert = true,
     -- diagnostics_indicator = function(count, level, diagnostics_dict, context)
     --   return "("..count..")"
     -- end,
@@ -58,7 +58,7 @@ bufferline.setup {
     -- end,
     offsets = { { filetype = "NvimTree", text = "", padding = 0 } },
     show_buffer_icons = true,
-    show_buffer_close_icons = false,
+    show_buffer_close_icons = true,
     show_close_icon = true,
     show_tab_indicators = true,
     persist_buffer_sort = true, -- whether or not custom sorted buffers should persist
