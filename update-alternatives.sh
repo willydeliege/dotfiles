1a3d47f75b9c273d68f83cbd31fd99cc4bea340d
Frédéric Willemm <frederic.willem@gmail.com>
2022-08-27 11:14:59 +0200

    add shebang to update alternatives

--- update-alternatives.sh
+++ update-alternatives.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 sudo update-alternatives --install /usr/bin/vi vi /usr/local/bin/nvim 59
 echo | sudo update-alternatives --config vi
 sudo update-alternatives --install /usr/bin/vim vim /usr/local/bin/nvim 60
