60ada4bf1048616438f50686d7e092a883049ff3
Frédéric Willem <frederic.willem@gmail.com>
2022-12-03 12:44:53 +0100

    change backround to true black

--- nvim/.config/nvim/lua/config/theme.lua
+++ nvim/.config/nvim/lua/config/theme.lua
@@ -1,6 +1,13 @@
 local M = {}
 
 function M.setup()
+  local dracula = require "dracula"
+  dracula.setup {
+    -- customize dracula color palette
+    colors = {
+      bg = "#000000",
+    },
+  }
   -- Lua:
   vim.cmd [[colorscheme dracula]]
 end
