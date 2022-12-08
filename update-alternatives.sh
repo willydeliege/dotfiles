ba713ab4da1bd4fe8d17cf8c85f37c275f745b9f
Frédéric Willem <frederic.willem@gmail.com>
2022-12-08 07:24:43 +0100

    fix typo

--- update-alternatives.sh
+++ update-alternatives.sh
@@ -4,5 +4,5 @@ sudo update-alternatives --install /usr/bin/vi vi /usr/local/bin/nvim 59
 echo | sudo update-alternatives --config vi
 sudo update-alternatives --install /usr/bin/vim vim /usr/local/bin/nvim 60
 echo | sudo update-alternatives --config vim
-sudo update-alternatives --install /usr/bin/editor editor /usr/loca/bin/nvim 60
+sudo update-alternatives --install /usr/bin/editor editor /usr/local/bin/nvim 60
 echo | sudo update-alternatives --config editor
