a49bc7685da73f58ad885a66fe0a0680352ee4d6
Frédéric Willem <frederic.willem@gmail.com>
2022-12-12 10:43:34 +0100

    add icons to standards exa options

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -7,7 +7,7 @@ if status is-interactive
   set -xg TERM xterm-256color
   set -xg TMPDIR /tmp
   set -xg PULSE_SERVER 127.0.0.1
-  set -xg MOZ_FAKE_NO_SANDBOX 1
   set -xg ZK_NOTEBOOK_DIR ~/willydeliege/
+  set -Ux EXA_STANDARD_OPTIONS --icons
   fzf_configure_bindings --git_status=\cS --directory=\cF --git_log=\cO  
 end
