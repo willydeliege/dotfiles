cc73eabc22df252c7ee77971215b5a63146ecb8d
Frédéric Willem <frederic.willem@gmail.com>
2024-05-20 20:45:33 +0200

    Initial commit

---
+++ dot_config/hypr/scripts/executable_init-wallpaper-engine.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+    # swww
+    echo ":: Using swww"
+    swww init
+    swww-daemon --format xrgb
+    sleep 0.5
+    ~/.config/hypr/scripts/wallpaper.sh init
+ 
---
+++ dot_config/hypr/scripts/executable_showkeys.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Format JSON proper;y
+JSON=$(hyprkeys --from-ctl --json | jq -r --slurp "[.[]][0]");
+
+USER_SELECTED=$(echo $JSON | jq -r 'range(0, length) as $i | "\($i) \(.[$i].mods) \(.[$i].key) \(.[$i].dispatcher) \(.[$i].arg)"' | rofi -dmenu -p 'Keybinds' -config ~/dotfiles/rofi/config-compact.rasi | awk -F ' ' '{print $1}')
+
+if [ -z "$USER_SELECTED" ]; then
+    exit 0;
+fi
+
+EVENT=$(echo $JSON | jq -r "[.[]] | .[$USER_SELECTED]" | jq -r '"\(.dispatcher) \(.arg)"');
+
+hyprctl dispatch "$EVENT";
