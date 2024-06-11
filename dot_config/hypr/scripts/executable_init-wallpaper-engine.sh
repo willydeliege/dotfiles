81cb1a11c24a140af12d33ca5d29e26178ada623
Frédéric Willem <frederic.willem@gmail.com>
2024-06-11 06:28:16 +0200

    don't change wallpaper at start up

--- dot_config/hypr/scripts/executable_init-wallpaper-engine.sh
+++ dot_config/hypr/scripts/executable_init-wallpaper-engine.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-    # swww
-    echo ":: Using swww"
-    swww init
-    swww-daemon --format xrgb
-    sleep 0.5
-    ~/.config/hypr/scripts/wallpaper.sh 
- 
+# swww
+echo ":: Using swww"
+swww-daemon --format xrgb
+sleep 0.5
+~/.config/hypr/scripts/wallpaper.sh init
