c41df6e1afca03fca4f05b9882e2c34bb6ebf544
Frédéric Willem <frederic.willem@gmail.com>
2024-06-26 21:44:48 +0200

    fix whitespaces

--- dot_config/doom/config.el
+++ dot_config/doom/config.el
@@ -4,7 +4,8 @@
 (setq user-full-name "Frédéric Willem"
       user-mail-address "frederic.willem@gmail.com")
 
-(setq doom-font "FiraCode Nerd Font-11" )
+(setq doom-font "FiraCode Nerd Font-11"
+      doom-symbol-font "Symbols Nerd Font")
 
 (setq doom-theme 'ef-duo-dark)
 
@@ -13,20 +14,33 @@
 (setq display-line-numbers-type 'relative)
 
 (+global-word-wrap-mode +1)
-
+(setq projectile-project-search-path '(( "~/Repos/" . 1 )))
+(after! orderless
+  (add-to-list 'completion-styles 'flex t))
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/org/")
 (setq org-agenda-files (list "~/org/gtd"))
+(setq denote-directory org-directory)
 (after! org
   (setq org-gtd-inbox-file "~/org/gtd/0-inbox.org")
   (setq org-capture-templates
         '(("i" "Inbox" entry
            (file org-gtd-inbox-file)
            "* %?\n%i\n%a"))))
-(add-hook! dired-mode #'dired-hide-details-mode)
-(setenv "WAYLAND_DISPLAY" ":1")
 
+(setq org-agenda-custom-commands
+      '(("n" "Agenda and all NEXT tasks"
+         ((agenda "" nil)
+          (todo "NEXT" nil))
+         nil)))
+
+(add-hook! dired-mode #'dired-hide-details-mode)
+
+(add-hook! dired-mode #'dired-hide-details-mode)
+(defvar org-gtd-archive-file "~/org/gtd/_gtd_archive_2024")
+(setq org-archive-location (concat org-gtd-archive-file "::datetree/"))
+(setq org-log-done 'time)
 ;;
 ;;; Colemak almost friendly
 
@@ -119,4 +133,24 @@
 ;;
 ;;; Calendar
 
-(setq      org-gcal-fetch-file-alist '(("frederic.willem@gmail.com" .  "~/org/gtd/calendar.org")))
+;; (setq      org-gcal-fetch-file-alist '(("frederic.willem@gmail.com" .  "~/org/gtd/calendar.org")))
+
+;;
+;;; Evil
+
+(after! evil
+  (defun +evil-paste-above ()
+    "Paste the line above."
+    (interactive)
+    (evil-insert-newline-above)
+    (evil-paste-after 1 evil-this-register))
+
+  (defun +evil-paste-below ()
+    "Paste the line below."
+    (interactive)
+    (evil-insert-newline-below)
+    (evil-paste-after 1 evil-this-register))
+  (map!
+   :map evil-normal-state-map
+   "[ p" #'+evil-paste-above
+   "] p" #'+evil-paste-below ))
--- dot_config/doom/custom.el
+++ dot_config/doom/custom.el
@@ -3,12 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(lsp-warn-no-matched-clients nil)
- '(org-agenda-custom-commands
-   '(("n" "Agenda and all TODOs"
-      ((agenda "" nil)
-       (todo "NEXT" nil))
-      nil))))
+ '(lsp-warn-no-matched-clients nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
