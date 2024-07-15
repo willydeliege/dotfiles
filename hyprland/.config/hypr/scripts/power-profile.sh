61d3e7b0f6975f3e76f484f6f3d0e79097762670
Frédéric Willem <frederic.willem@gmail.com>
2024-07-15 05:48:11 +0200

    rofi dmenu config

--- hyprland/.config/hypr/scripts/power-profile.sh
+++ hyprland/.config/hypr/scripts/power-profile.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-options=$(printf "power-saver\nbalanced\nperformance" | rofi -dmenu -p "Power Profile")
+options=$(printf "power-saver\nbalanced\nperformance" | rofi -dmenu -p "Power Profile" -config ~/.config/rofi/config-dmenu.rasi)
 
 sudo powerprofilesctl set "$options"
