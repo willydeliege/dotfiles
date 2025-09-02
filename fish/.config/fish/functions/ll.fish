05ea882822751e68f1e0e4557c5e361010715fe5
Frédérix Willem <frederic.willem@gmail.com>
2025-09-02 14:43:29 +0200

    add emacs and pisces (autoclose parens)

--- fish/.config/fish/functions/ll.fish
+++ fish/.config/fish/functions/ll.fish
@@ -1,4 +1,4 @@
-function ll --wraps='eza -l --color=always --group-directories-first --icons' --wraps=eza_git --description 'alias ll eza_git'
+function ll --wraps=eza_git --description 'alias ll eza_git'
   eza_git $argv
         
 end
