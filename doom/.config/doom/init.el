f40abd276897b309c92f9c1730c9fe0dc1f74210
Frédérix Willem <frederic.willem@gmail.com>
2025-09-17 10:02:46 +0200

    remove unused doom modules

--- doom/.config/doom/init.el
+++ doom/.config/doom/init.el
@@ -74,7 +74,7 @@
        vc                ; version-control and Emacs, sitting in a tree
 
        :term
-       eshell            ; the elisp shell that works everywhere
+       ;; eshell            ; the elisp shell that works everywhere
        ;;shell             ; simple shell REPL for Emacs
        ;;term              ; basic terminal emulator for Emacs
        vterm             ; the best terminal emulation in Emacs
@@ -152,7 +152,7 @@
        ;;nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
-       (org +passwords +contacts +crypt +pandoc +present +roam2 +pretty)   ; organize your plain life in ext
+       (org +passwords +contacts +crypt +pandoc +roam2 +pretty)   ; organize your plain life in ext
        ;;php               ; perl's insecure younger brother
        plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
