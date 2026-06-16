1ef8c100b7bea20644bc748f085be23c7c4e2de3
Frédéric Willem <frederic.willem@gmail.com>
2026-06-16 09:03:31 +0200

    [zsh] add fzt-tab(-source)

--- zshrc/.config/zsh/plugins.zsh
+++ zshrc/.config/zsh/plugins.zsh
@@ -1,17 +1,15 @@
-
 ### Added by Zinit's installer
 if [[ ! -f $HOME/.local/share/zinit/zinit.git/zinit.zsh ]]; then
-    print -P "%F{33} %F{220}Installing %F{33}ZDHARMA-CONTINUUM%F{220} Initiative Plugin Manager (%F{33}zdharma-continuum/zinit%F{220})…%f"
-    command mkdir -p "$HOME/.local/share/zinit" && command chmod g-rwX "$HOME/.local/share/zinit"
-    command git clone https://github.com/zdharma-continuum/zinit "$HOME/.local/share/zinit/zinit.git" && \
-        print -P "%F{33} %F{34}Installation successful.%f%b" || \
-        print -P "%F{160} The clone has failed.%f%b"
+  print -P "%F{33} %F{220}Installing %F{33}ZDHARMA-CONTINUUM%F{220} Initiative Plugin Manager (%F{33}zdharma-continuum/zinit%F{220})…%f"
+  command mkdir -p "$HOME/.local/share/zinit" && command chmod g-rwX "$HOME/.local/share/zinit"
+  command git clone https://github.com/zdharma-continuum/zinit "$HOME/.local/share/zinit/zinit.git" \
+    && print -P "%F{33} %F{34}Installation successful.%f%b" \
+    || print -P "%F{160} The clone has failed.%f%b"
 fi
 
 source "$HOME/.local/share/zinit/zinit.git/zinit.zsh"
 autoload -Uz _zinit
-(( ${+_comps} )) && _comps[zinit]=_zinit
-
+((${+_comps})) && _comps[zinit]=_zinit
 
 # Load Oh My Zsh plugins
 zinit snippet OMZL::git.zsh
@@ -21,7 +19,8 @@ zinit wait lucid for \
   OMZP::sudo \
   OMZP::extract \
   zsh-users/zsh-syntax-highlighting \
-  zsh-users/zsh-autosuggestions
-
+  zsh-users/zsh-autosuggestions \
+  Aloxaf/fzf-tab \
+  Freed-Wu/fzf-tab-source
 
 zinit light romkatv/powerlevel10k
