071dbe7bf15b6d923a171bf902f2af348be2bf45
Frédéric Willem <frederic.willem@gmail.com>
2026-06-17 07:08:50 +0200

    use yazi ipo oil

---
+++ nvim/.config/nvim/lua/plugins/file-manager.lua
@@ -0,0 +1,36 @@
+---@type LazySpec
+return {
+  "mikavilpas/yazi.nvim",
+  event = "VeryLazy",
+  dependencies = {
+    { "nvim-lua/plenary.nvim", lazy = true },
+  },
+  keys = {
+    -- 👇 in this section, choose your own keymappings!
+    {
+      "-",
+      mode = { "n", "v" },
+      "<cmd>Yazi<cr>",
+      desc = "Open yazi at the current file",
+    },
+    {
+      -- Open in the current working directory
+      "<leader>fe",
+      "<cmd>Yazi cwd<cr>",
+      desc = "File explorer (cwd)",
+    },
+    {
+      "<leader>ty",
+      "<cmd>Yazi toggle<cr>",
+      desc = "Resume the last yazi session",
+    },
+  },
+  ---@type YaziConfig | {}
+  opts = {
+    -- if you want to open yazi instead of netrw, see below for more info
+    open_for_directories = true,
+    keymaps = {
+      show_help = "<f1>",
+    },
+  },
+}
