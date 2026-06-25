7a8dc9fca8f22571aee949268867d189e3f62a89
Frédéric Willem <frederic.willem@gmail.com>
2026-06-25 10:51:29 +0200

    fine tune sesh

--- zshrc/.config/zsh/bindings.zsh
+++ zshrc/.config/zsh/bindings.zsh
@@ -31,7 +31,7 @@ function sesh-sessions() {
     exec </dev/tty
     exec <&1
     local session
-    session=$(sesh list -t -c -z | fzf --height 40% --reverse --border-label ' sesh ' --border --prompt '⚡  ')
+    session=$(sesh list -t -c -z --hide-duplicates | fzf --height 40% --reverse --border-label ' sesh ' --border --prompt '⚡  ')
     zle reset-prompt >/dev/null 2>&1 || true
     [[ -z "$session" ]] && return
     sesh connect $session
