c7a7261a795d09bab043ae8ef9ebda89b604229f
Frédérix Willem <frederic.willem@gmail.com>
2026-07-20 10:29:35 +0200

    [nvim] use web-devicons ipo mini

--- nvim/.config/nvim/lua/plugins/extras.lua
+++ nvim/.config/nvim/lua/plugins/extras.lua
@@ -3,6 +3,9 @@
 -- =============================================================================
 return {
   {
+    "nvim-tree/nvim-web-devicons",
+  },
+  {
     "kylechui/nvim-surround",
     version = "^4.0.0", -- Use for stability; omit to use `main` branch for the latest features
     event = "VeryLazy",
